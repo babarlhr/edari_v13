8f1ebd7e1b1b52ec48f48453fe50160648ef2169
Ecube <danish_ali89@hotmail.com>
2020-01-30 15:26:53 +0500

    Job Position Extension

--- job_positions_extension/models/job_position.py
+++ job_positions_extension/models/job_position.py
@@ -13,24 +13,41 @@ class JobsExtension(models.Model):
 	anticipated_start_date = fields.Date(string="Anticipated Start Date")
 	budget = fields.Float(string="Budget")
 	contract_length = fields.Float(string="Contract Length")
-	
+	template = fields.Many2one('costcard.template', string="Template")
+	# so_count = fields.Integer(compute='_compute_so_count', string='Cost Card Count')
 	visa_entity = fields.Many2one('visa.entity', string="Visa Entity")
 	annual_leaves = fields.Float(string="Annual Leaves")
 	sick_leaves = fields.Float(string="Sick Leaves")
 
 	probation_period = fields.Selection([
-        ('three','3 months'),
-        ('six','6 months'),
-        ], string='Probation Period', default='three')
+		('three','3 months'),
+		('six','6 months'),
+		], string='Probation Period', default='three')
 	notice_period = fields.Selection([
-        ('one','1 months'),
-        ('three','3 months'),
-        ], string='Notice Period', default='one')
+		('one','1 months'),
+		('three','3 months'),
+		], string='Notice Period', default='one')
 	working_days = fields.Selection([
-        ('five','5 days'),
-        ('six','6 days'),
-        ], string='Working Days', default='five')
-    
+		('five','5 days'),
+		('six','6 days'),
+		], string='Working Days', default='five')
+
+
+	# def _compute_so_count(self):
+	# 	# so_data = self.env['sale.order'].sudo().read_group([('job_pos','in',self.ids)],['job_pos'],['job_pos'])
+	# 	# result = dict((data['job_pos'][0], data['so_id_count']) for data in so_data)
+	# 	for jobs in self:
+	# 		print ("XXXXXXXXXXXXXXXXXXXXXXXXX")
+	# 		print ("XXXXXXXXXXXXXXXXXXXXXXXXX")
+	# 		print (jobs.id)
+	# 		recs = self.env['sale.order'].search(['job_pos','=',jobs.id])
+	# 		print (recs)
+	# 		print (count)
+	# 		print (type(count))
+	# 		print ("XXXXXXXXXXXXXXXXXXXXXXXXX")
+	# 		print ("XXXXXXXXXXXXXXXXXXXXXXXXX")
+	# 		jobs.so_count = int(count)
+	
 
 	def so_smart_button(self):
 		print ('XXXXXXXXXXXXXXXXXXXXXXXX')
@@ -50,6 +67,13 @@ class JobsExtension(models.Model):
 		 'view_type': 'form',
 		 'view_mode': 'tree,form',
 		 # 'views': [(view_id_tree[0].id, 'tree'),(False,'form')],
+		 'context': {
+		 'default_job_pos':self.id,
+		 'default_template':self.template.id,
+		 'default_partner_id':self.customer.id,
+		 'default_no_of_months':self.contract_length,
+		 'default_per_month_gross_salary':self.budget,
+		 },
 		 'view_id ref=" sale.view_quotation_tree_with_onboarding"': '',
 		 'target': 'current',
 		 'domain': domain,
