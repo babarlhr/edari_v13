604f99fb48166ad29bb301581fcd7d64eae24aea
Younis <m.younis.mahsud@gmail.com>
2020-05-11 16:33:50 +0500

    Commented code

--- edari_timesheet/edari_timesheet/models/contact.py
+++ edari_timesheet/edari_timesheet/models/contact.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 class Contact(models.Model):
     _inherit = 'res.partner'
 
-    invite_token = fields.Char('Invite Token')
-
-    def generate_invite_token(self):
-        self.invite_token = secrets.token_urlsafe(32)
+    # invite_token = fields.Char('Invite Token')
+    #
+    # def generate_invite_token(self):
+    #     self.invite_token = secrets.token_urlsafe(32)
