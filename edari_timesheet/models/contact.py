60019f7c8fd71ee1814711190fe2dd6d86b382ce
Younis <m.younis.mahsud@gmail.com>
2020-05-11 15:10:51 +0500

    edari timesheet updated module

---
+++ edari_timesheet/models/contact.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api
+import logging, secrets
+
+_logger = logging.getLogger(__name__)
+
+class Contact(models.Model):
+    _inherit = 'res.partner'
+
+    invite_token = fields.Char('Invite Token')
+    portal_uid = fields.Char('Portal UID')
+    invite_url = fields.Char(compute='get_invite_url')
+
+    def generate_invite_token(self):
+        if self.portal_uid == False and self.invite_token == False:
+            self.invite_token = secrets.token_urlsafe(32)
+
+    @api.depends('invite_token')
+    def get_invite_url(self):
+        for record in self:
+            record.invite_url = False
+            if record.invite_token:
+                record.invite_url = "https://clients.edari.io?type=customer&invite_token={}".format(
+                    self.invite_token)
