1516d14627ea6ffb5e2484f743686644b52cf6e4
Camille Lahoud <camille.lahoud@edarione.com>
2020-05-13 16:05:23 +0300

    Fixed bug in SO Name

--- so_extension/models/so_ext_model.py
+++ so_extension/models/so_ext_model.py
@@ -114,8 +114,10 @@ class SaleOrderExt(models.Model):
 		applicant_name = ""
 		if self.contract:
 			applicant_name = self.contract.employee_id.name
+		elif self.job_pos:
+			applicant_name = self.job_pos.partner_name
 		else:
-			applicant_name = self.applicant_name.partner_name
+			applicant_name = self.partner_id.partner_name
 		if not self.sequence_number:
 			self.sequence_number = self.name
 		self.name = self.sequence_number + "-" + cost_card_name + "-" + applicant_name
