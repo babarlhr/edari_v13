4a72419a4be401df383ce32f3dfd7d2d4e7cf70b
Camille Lahoud <camille.lahoud@edarione.com>
2020-05-13 16:07:25 +0300

    Fixed bug in UpdateSOName

--- so_extension/models/so_ext_model.py
+++ so_extension/models/so_ext_model.py
@@ -115,7 +115,7 @@ class SaleOrderExt(models.Model):
 		if self.contract:
 			applicant_name = self.contract.employee_id.name
 		elif self.job_pos:
-			applicant_name = self.job_pos.partner_name
+			applicant_name = self.job_pos.customer.partner_name
 		else:
 			applicant_name = self.partner_id.partner_name
 		if not self.sequence_number:
