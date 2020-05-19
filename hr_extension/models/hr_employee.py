67ecfc039d08a448f825e6ec385cf74196824afa
Ecube <danish_ali89@hotmail.com>
2020-05-19 23:25:54 +0500

    Employee reference in Journal Entry Payslip

--- hr_extension/models/hr_employee.py
+++ hr_extension/models/hr_employee.py
@@ -13,6 +13,7 @@ class HrEmployeeExtension(models.Model):
 	employee_code = fields.Char("Employee Code")
 	cost_card = fields.Many2one("sale.order", "Cost Card")
 	customer = fields.Many2one('res.partner' , 'Customer')
+	partner_id =fields.Many2one('res.partner',"Partner ID")
 	wage = fields.Float("Wage")
 
 	employee_type = fields.Selection([
@@ -23,13 +24,24 @@ class HrEmployeeExtension(models.Model):
 
 	@api.model
 	def create(self, vals):
+		
 		new_record = super(HrEmployeeExtension, self).create(vals)
 		# updating wage in employee
 		new_record.AllocateLeaves()
 		for x in new_record.contract_ids:
 			if x.state == 'open':
 				x.wage = new_record.wage
+		partner_id = self.env['res.partner'].create([{'name': new_record.name,
+										'phone':new_record.work_phone,
+										'email':new_record.work_email,
+										'mobile':new_record.mobile_phone,
+										'is_employee':True}])
+		new_record.partner_id = partner_id.id
 		return new_record
+
+	@api.onchange('name')
+	def namechange(self):
+		self.partner_id.write({'name':self.name})
 
 	def write(self, vals):
 		rec = super(HrEmployeeExtension, self).write(vals)
@@ -39,6 +51,13 @@ class HrEmployeeExtension(models.Model):
 			for x in self.contract_ids:
 				if x.state == 'open':
 					x.wage = vals['wage']
+		
+		if self.active == True:
+			# record.active =False
+			self.partner_id.write({'active':True})
+		if self.active == False:
+			self.partner_id.write({'active':False})			
+
 		return rec
 
 
@@ -75,8 +94,9 @@ class HrEmployeeExtension(models.Model):
 			})
 		create_allocation.action_approve()
 
+class res_partner_customized(models.Model):
 
-
-
+	_inherit = 'res.partner'
+	is_employee = fields.Boolean("Employee")
 
 
---
+++ hr_extension/models/hr_payslip.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from datetime import timedelta,datetime,date
+from odoo.exceptions import Warning, ValidationError
+from dateutil.relativedelta import *
+
+		
+class HRPayslipExt(models.Model):
+	_inherit = 'hr.payslip'
+
+	def action_payslip_done(self):
+
+		res = super(HRPayslipExt, self).action_payslip_done()
+
+		if self.move_id:
+			if not self.move_id.state == 'cancel':
+				if self.move_id.state == 'posted':
+					self.move_id.button_draft()
+				for x in self.move_id.line_ids:
+					x.partner_id = 14
+
+		return res
+
+	
+
+		
+
+
+
+
+		
