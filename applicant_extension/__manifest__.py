5112bfaebe7f570a01a300104c5425f4848d840c
Ecube <danish_ali89@hotmail.com>
2020-06-11 16:57:34 +0500

    Main Pull

--- applicant_extension/__manifest__.py
+++ applicant_extension/__manifest__.py
@@ -9,7 +9,7 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
+    # any modules necessary for this one to work correctly
     'depends': ['base','hr_recruitment','sale'],
 
     # always loaded
