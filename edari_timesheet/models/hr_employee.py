c6450c63c533f8e222be6a8c16fac664e938cabe
Irshad kk <irshad.kk@basealpha.io>
2020-07-13 13:12:22 +0400

    reverted to sending mail with template

--- edari_timesheet/models/hr_employee.py
+++ edari_timesheet/models/hr_employee.py
@@ -17,38 +17,38 @@ class Employee(models.Model):
             self.invite_token = secrets.token_urlsafe(32)
             # sending email with template
             # fetch the template id for sending the mail
-            # template_id = self.env.ref('edari_timesheet.edari_portal_invite_email_template').id
-            # template = self.env['mail.template'].browse(template_id)
-            # template.send_mail(self.id, force_send=True)
+            template_id = self.env.ref('edari_timesheet.edari_portal_invite_email_template').id
+            template = self.env['mail.template'].browse(template_id)
+            template.send_mail(self.id, force_send=True)
 
             # Sending email without a template
-            subject = "Edari Portal Invitation URL "
-            # name = self.name
-            body_html = """Dear Thank you.<br/>"""
-            # email_to = self.work_email
-            email_to = "irshadcom.26@gmail.com"
-            vals = {
-                'email_from': email_to,
-                'email_to': email_to,
-                'subject': subject,
-                'body_html': body_html,
-                'state': 'outgoing',
-                'auto_delete': False,
-            }
-            mail_id = self.env['mail.mail'].create(vals)
-            new_vals = {
-                'email_from': vals.get('email_from', False),
-                'subject': vals.get('subject', False),
-                'message_type': 'comment',
-                'model': 'hr.employee',
-                'parent_id': self.id,
-                'record_name': self.name,
-                'body': vals.get('body_html'),
-                'res_id': self.id,
-                'subtype_id': self.env.ref('mail.mt_note').id,
-            }
-            self.env['mail.message'].create(new_vals)
-            mail_id.send()
+            # subject = "Edari Portal Invitation URL "
+            # # name = self.name
+            # body_html = """Dear Thank you.<br/>"""
+            # # email_to = self.work_email
+            # email_to = "irshadcom.26@gmail.com"
+            # vals = {
+            #     'email_from': email_to,
+            #     'email_to': email_to,
+            #     'subject': subject,
+            #     'body_html': body_html,
+            #     'state': 'outgoing',
+            #     'auto_delete': False,
+            # }
+            # mail_id = self.env['mail.mail'].create(vals)
+            # new_vals = {
+            #     'email_from': vals.get('email_from', False),
+            #     'subject': vals.get('subject', False),
+            #     'message_type': 'comment',
+            #     'model': 'hr.employee',
+            #     'parent_id': self.id,
+            #     'record_name': self.name,
+            #     'body': vals.get('body_html'),
+            #     'res_id': self.id,
+            #     'subtype_id': self.env.ref('mail.mt_note').id,
+            # }
+            # self.env['mail.message'].create(new_vals)
+            # mail_id.send()
 
     @api.depends('invite_token')
     def get_invite_url(self):
