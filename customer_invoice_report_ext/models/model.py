f31eaf86de6f052d6ef4985a493fd11fc4a24b0f
Yasir <yasirkrauf@yahoo.com>
2020-08-17 13:53:40 +0500

    customer invoice report
    (cherry picked from commit fbfc40f7507d3529666f7cd38300b99edd465a91)

---
+++ customer_invoice_report_ext/models/model.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+########################################################################################
+########################################################################################
+##                                                                                    ##
+##    Odoo13                                                                          ##
+##    Copyright (C) 2019 odoo13  (<http://ecube.pk>). All Rights Reserved             ##
+##    contact us  at                                   for your erp needs.            ##
+## Odoo is an all-in-one business software including CRM, e-commerce, accounting,MRP, ##
+## Project management, and inventory. It helps you to improve the quality and         ##
+## efficiency of your business.                                                       ##
+########################################################################################
+########################################################################################
+
+from odoo import models, fields, api
+from datetime import date
+from datetime import datetime, date, timedelta
+from odoo.exceptions import Warning, ValidationError, UserError
