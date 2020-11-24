5909842e8e68e8ec22195f08a3672092acb8da78
Camille Lahoud <camille.lahoud@basealpha.io>
2020-11-24 12:25:54 +0200

    Fix journal item name

--- so_extension/models/so_ext_model.py
+++ so_extension/models/so_ext_model.py
@@ -761,7 +761,7 @@ class SaleOrderExt(models.Model):
 			if mv.account_id.id not in merged:
 				name = mv.name
 				if mv.account_id.internal_group == 'income':
-					name = "{} - {}".format(self.ref, self.invoice_month)
+					name = "{} - {}".format(self.name, self.invoice_month)
 				merged[mv.account_id.id] = {
 					'account_id':mv.account_id.id,
 					'name': name,
