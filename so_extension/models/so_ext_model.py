b85519f6adddba1a60a0995b6ecbf62758c6fd59
Camille Lahoud <camille.lahoud@basealpha.io>
2020-11-24 20:21:14 +0200

    Fix tax ids

--- so_extension/models/so_ext_model.py
+++ so_extension/models/so_ext_model.py
@@ -805,10 +805,10 @@ class SaleOrderExt(models.Model):
 		})
 
 		invoice = self.env['account.move'].with_context(default_type='out_invoice').search([('id','=',moves.id)],limit=1)
+		# invoice[0]._recompute_dynamic_lines(recompute_all_taxes=True)
 		for line in invoice[0].line_ids:
 			line._onchange_mark_recompute_taxes()
 			line._onchange_account_id()
-		invoice[0]._recompute_dynamic_lines(recompute_all_taxes=True)
 
 		return moves
 
